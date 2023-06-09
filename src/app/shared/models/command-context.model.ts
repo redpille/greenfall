7f425706f225bfc79dd286f64ab6108744439abb
mrbrique <locus.chan.dev@gmail.com>
2023-06-09 13:04:40 +0100

    implement CommandContext.generate()

--- src/app/shared/models/command-context.model.ts
+++ src/app/shared/models/command-context.model.ts
@@ -7,4 +7,19 @@ export class CommandContext {
     tickRate: number = 0;
     // how frequent the command mutate
     mutationRate: number = 0;
+
+    constructor(header: number, length: number, tickRate: number, mutationRate: number) {
+        this.header = header;
+        this.length = length;
+        this.tickRate = tickRate;
+        this.mutationRate = mutationRate;
+    }
+
+    public static generate(): CommandContext {
+        var header = Math.ceil(Math.random() * 2);
+        var length = Math.floor(Math.random() * 20) + 4;
+        var tickRate = Math.pow(2, Math.ceil(Math.random() * 4));
+        var mutationRate = 0;
+        return new CommandContext(header, length, tickRate, mutationRate);
+    }
 }
